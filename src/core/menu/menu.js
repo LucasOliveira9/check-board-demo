be43856d14a7c924f968d6ef40e0940d0ea7172f
Lucas Oliveira Rodrigues <lucasolliveiraa1@gmail.com>
2026-02-11 13:37:01 -0300

    chore: remove test slice from fen stream loading

--- src/core/menu/menu.js
+++ src/core/menu/menu.js
@@ -74,7 +74,7 @@ class MenuRuntime {
             this.loadingListenerOff();
         });
         this.app.current.setLoadState(true);
-        this.app.current.getClient()?.loadFenStream(testFen.slice(0, 50));
+        this.app.current.getClient()?.loadFenStream(testFen);
         this.app.current.clearUndoRedo();
         this.onSetLoading(true);
     }
